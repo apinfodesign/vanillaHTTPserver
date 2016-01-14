fd7d9f2094d74872ab7adbd744aaec0d9e4a1a31
Miles Hochstein <miles.hochstein@gmail.com>
2016-01-13 19:40:32 -0800

    first files

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Vanilla</title>
+</head>
+<body>
+    <h1>Welcome to the vanilla page!</h1>
+    
+    <p>You requested url via method</p>
+    <p>You're query params were qs</p>
+    
+</body>
+</html>
